480fa998a56ef59a5599a040aacd833e8728b15f
Kye Clevenger <KyeClevenger@gmail.com>
2024-02-23 07:42:39 -0700

    friday

--- script.js
+++ script.js
@@ -194,7 +194,7 @@
 
 function updateDateTime() {
     var currentDate = new Date();
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     var formattedDate = currentDate.toLocaleDateString('en-US', options);
 
     document.getElementById('datetime').innerHTML = formattedDate;
